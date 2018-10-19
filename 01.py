ca0f1f96f3221270659c3b80fa9600811d6cf229
g i b <Gibby@Gibbys-MacBook-Pro.local>
2018-10-19 10:09:19 -1000

    Testing...

---
+++ 01.py
@@ -0,0 +1,5 @@
+current_year = 2018
+person_age = int(input("Please enter your age: "))
+person_name = raw_input("Please enter your name: ")
+til_100 = int(100 - person_age) + current_year
+print(person_name + " you will turn 100 years old in " + str(til_100) + ".")
